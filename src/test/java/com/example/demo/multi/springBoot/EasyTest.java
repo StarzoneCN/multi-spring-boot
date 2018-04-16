c36091b6e0c994c07b4b4a922cc4af2f6b2dfcb5
LiHongxing <lihongxing@bluemoon.com.cn>
2018-04-16 20:23:25 +0800

    no message

---
+++ src/test/java/com/example/demo/multi/springBoot/EasyTest.java
@@ -0,0 +1,17 @@
+package com.example.demo.multi.springBoot;
+
+import org.junit.Test;
+
+/**
+ * @author: LiHongxing
+ * @email: lihongxing@bluemoon.com.cn
+ * @date: Create in 2018/4/16 11:52
+ * @modefied:
+ */
+public class EasyTest {
+
+    @Test
+    public void test(){
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
