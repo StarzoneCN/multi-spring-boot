3e14ebeac8f05ff5257d4883173d3e1bef55bf31
LiHongxing <lihongxing@bluemoon.com.cn>
2018-11-21 23:00:21 +0800

    no message

--- src/main/java/com/example/demo/multi/springBoot/mybatisPlus/entity/User.java
+++ src/main/java/com/example/demo/multi/springBoot/mybatisPlus/entity/User.java
@@ -1,10 +1,12 @@
 package com.example.demo.multi.springBoot.mybatisPlus.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import java.io.Serializable;
+import java.util.Date;
+
+import com.example.demo.multi.springBoot.mybatisPlus.enums.PhoneEnum;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
