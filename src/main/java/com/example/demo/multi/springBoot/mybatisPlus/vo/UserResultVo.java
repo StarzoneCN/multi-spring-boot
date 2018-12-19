e4a22c0680bf973974a6045be87a74b32a30438e
LiHongxing <lihongxing@bluemoon.com.cn>
2018-12-19 16:42:17 +0800

    no message

---
+++ src/main/java/com/example/demo/multi/springBoot/mybatisPlus/vo/UserResultVo.java
@@ -0,0 +1,42 @@
+package com.example.demo.multi.springBoot.mybatisPlus.vo;
+
+/**
+ * vo
+ *
+ * @author: LiHongxing
+ * @email: lihongxing@bluemoon.com.cn
+ * @date: Create in 2018/12/14 14:11
+ * @modefied:
+ */
+public class UserResultVo {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/test/java/com/example/demo/multi/springBoot/CodeGenerator.java
+++ src/test/java/com/example/demo/multi/springBoot/CodeGenerator.java
@@ -1,10 +1,15 @@
 package com.example.demo.multi.springBoot;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.querys.MySqlQuery;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import org.apache.commons.lang3.StringUtils;
@@ -21,11 +26,12 @@ import java.util.List;
  * @modefied:
  */
 public class CodeGenerator {
-    private static final String moduleName = "sys";
-    private static final String[] include_tables = {"^sys_.*"};
+    private static final String moduleName = "test";
+    private static final String[] include_tables = {"test_columns"}; // 需要生成代码的表格，支持正则
+    private static final String fieldPrefix = "is"; // 字段前缀
     private static final String logicDeleteField = "del_flag";
     private static final String author = "LiHongxing";
-    private static final String sourcesDir = "/src/main/java";
+    private static final String sourcesDir = "/src/test/java";
     private static final String classpath = "/src/main/resources";
     private static final String serviceName = "%sService";
 
@@ -53,6 +59,7 @@ public class CodeGenerator {
         String projectPath = System.getProperty("user.dir");
         gc.setOutputDir(projectPath + sourcesDir);
         gc.setAuthor(author);
+        /*是否打开输出目录*/
         gc.setOpen(false);
         /* 打开二级缓存，因为自动生成的代码操作的基本都是单表，对于单表，使用二级缓存是比较明智的选择 */
         gc.setEnableCache(true);
@@ -60,6 +67,12 @@ public class CodeGenerator {
         /* 定义service文件名，“%s”表示entityName，也就是实体名，“%S（大写）”表示大写的实体名，如：UserInfo→USERINFO */
         /* entity、 mapper、xml、serviceImpl、controller都可采用类似的方法自定义名称*/
         gc.setServiceName(serviceName);
+        /*是否开启activeRecord模式*/
+        gc.setActiveRecord(false);
+        /*是否生成baseResultMap*/
+        gc.setBaseResultMap(true);
+        /* 设置日期字段类型 */
+        gc.setDateType(DateType.TIME_PACK);
         autoGenerator.setGlobalConfig(gc);
 
         /*数据源配置*/
@@ -69,6 +82,8 @@ public class CodeGenerator {
         dsc.setDriverName(driverClassName);
         dsc.setUsername(databaseUsername);
         dsc.setPassword(databasePassword);
+        /*设置库查询sql*/
+        dsc.setDbQuery(new MySqlQuery());
         autoGenerator.setDataSource(dsc);
 
         /*包配置*/
@@ -98,7 +113,7 @@ public class CodeGenerator {
         }
         autoGenerator.setPackageInfo(pc);
 
-        // 自定义配置
+        // 注入配置
         InjectionConfig injectionConfig = new InjectionConfig() {
             @Override
             public void initMap() {
@@ -107,6 +122,12 @@ public class CodeGenerator {
         };
         List<FileOutConfig> focList = new ArrayList<>();
         focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+
+            /**
+             * 设置mapper.xml文件全路径
+             * @param tableInfo
+             * @return
+             */
             @Override
             public String outputFile(TableInfo tableInfo) {
                 String xmlFileName = tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
@@ -122,17 +143,27 @@ public class CodeGenerator {
             }
         });
         injectionConfig.setFileOutConfigList(focList);
+        injectionConfig.setFileCreate((configBuilder, fileType, filePath) -> {
+            switch (fileType) {
+                case CONTROLLER:
+                    return false;
+                default:
+                    return true;
+            }
+        });
         autoGenerator.setCfg(injectionConfig);
         autoGenerator.setTemplate(new TemplateConfig().setXml(null));
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
+        /*是否跳过视图*/
+        strategy.setSkipView(true);
+        /*数据库表映射到实体的命名策略*/
         strategy.setNaming(NamingStrategy.underline_to_camel);
+        /*数据库表字段映射到实体的命名策略, 未指定按照 naming 执行*/
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         /*配置实体类的超类*/
         // strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
-        /*是否使用lombok工具*/
-        strategy.setEntityLombokModel(true);
         /*使用restful风格*/
         strategy.setRestControllerStyle(true);
         /*配置controller的超类*/
@@ -140,15 +171,27 @@ public class CodeGenerator {
         /* 包含的表名，也可以使用exclude（排除表名），但是include/exclude只能二选一 */
         /*支持正则*/
         strategy.setInclude(include_tables);
-        /* 实体公共字段 */
-        // strategy.setSuperEntityColumns("id");
-        // strategy.setControllerMappingHyphenStyle(true);
-        /*表格前缀*/
+        /*表格前缀，这会自动去掉前缀，如：表名sys_user, 生成的实体是User*/
         strategy.setTablePrefix(pc.getModuleName() + "_");
+        /*效果与tablePrefix类似*/
+        strategy.setFieldPrefix(fieldPrefix + "_");
         /*去除“is_”前缀*/
         strategy.setEntityBooleanColumnRemoveIsPrefix(true);
+        /*(实体父类)公共字段，如果设置，那么entity中就不会生成这些字段的代码*/
+        // strategy.setSuperEntityColumns("create_time");
+        /*是否将表的字段名以常量的形式，放在entity类中*/
+        strategy.setEntityColumnConstant(true);
+        /*是否使用builder模式，如果使用了lombok模式，entityBuilderModel设置将失效*/
+        strategy.setEntityBuilderModel(false);
+        /*是否使用lombok工具*/
+        strategy.setEntityLombokModel(true);
         strategy.entityTableFieldAnnotationEnable(true);
+        /*逻辑删除标志字段*/
         strategy.setLogicDeleteFieldName(logicDeleteField);
+        /*自动填充配置*/
+        strategy.setTableFillList(new ArrayList<TableFill>(2){{
+            this.add(new TableFill("create_time", FieldFill.INSERT));
+        }});
         autoGenerator.setStrategy(strategy);
         /*设置模板*/
         autoGenerator.setTemplateEngine(new FreemarkerTemplateEngine());
