743aeca560bb886500dea297f7f0d57d14d0cc1b
LiHongxing <lihongxing@bluemoon.com.cn>
2019-01-04 11:00:35 +0800

    no message

--- src/main/java/com/example/demo/multi/springBoot/mojo/GreetingMojo.java
+++ src/main/java/com/example/demo/multi/springBoot/mojo/GreetingMojo.java
@@ -1,10 +1,12 @@
 package com.example.demo.multi.springBoot.mojo;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * @author: LiHongxing
@@ -14,10 +16,23 @@ import org.apache.maven.plugins.annotations.Mojo;
  */
 @Mojo(name = "printPc", defaultPhase = LifecyclePhase.COMPILE)
 public class GreetingMojo extends AbstractMojo {
+
+    @Parameter(property = "printTip", defaultValue = "true")
+    private boolean printTip;
+    @Parameter(property = "printPc.projectVersion", defaultValue = "${project.version}")
+    private String projectVersion;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("执行Greeting插件。。。");
+        if (this.printTip) {
+            getLog().info("执行Greeting插件。。。");
+        }
         getLog().info( getPluginContext().toString() );
-        getLog().info("--------------------------");
+        if (StringUtils.isNotBlank(this.projectVersion)){
+            getLog().info("projectVersion = " + projectVersion);
+        }
+        if (this.printTip) {
+            getLog().info("--------------------------");
+        }
     }
 }
