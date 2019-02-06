804752a1f072e7a115c160ae625cbd79291225b5
LiHongxing <lihongxing@bluemoon.com.cn>
2019-02-06 18:24:51 +0800

    no message

--- src/main/java/com/example/demo/multi/springBoot/DemoMultiSpringBootApplication.java
+++ src/main/java/com/example/demo/multi/springBoot/DemoMultiSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoMultiSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoMultiSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMultiSpringBootApplication.class, args);
+    }
 }
