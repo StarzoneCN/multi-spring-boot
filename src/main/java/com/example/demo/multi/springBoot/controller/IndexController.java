a622ae78f4b3b4bda17a766ae5dc2cf967f7d8fa
LiHongxing <lihongxing@bluemoon.com.cn>
2019-02-27 13:06:52 +0800

    no message

--- src/main/java/com/example/demo/multi/springBoot/controller/IndexController.java
+++ src/main/java/com/example/demo/multi/springBoot/controller/IndexController.java
@@ -1,7 +1,14 @@
 package com.example.demo.multi.springBoot.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -14,10 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class IndexController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
     private static final String MSG_APP_WELCOME = "欢迎来到Spring-Boot的世界！";
 
     @RequestMapping
     public String index(){
         return MSG_APP_WELCOME;
+    }
+
+    @RequestMapping("show/log")
+    public String showLog(String logStr){
+        Map<String, Object> map = new HashMap<>();
+        map.put("timestamp", System.currentTimeMillis());
+        map.put("msg", logStr);
+        LOGGER.error(JSONObject.toJSONString(map));
+        return JSONObject.toJSONString(map);
     }
 }
