d7edf48ee0c0236042272a88f9a0a4fc9fb1bfe6
starzonecn <starzonecn@gmail.com>
2019-06-10 17:23:16 +0800

    no message

--- src/main/java/com/example/demo/multi/springBoot/controller/TestWithoutAuthController.java
+++ src/main/java/com/example/demo/multi/springBoot/controller/TestWithoutAuthController.java
@@ -1,10 +1,5 @@
 package com.example.demo.multi.springBoot.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
